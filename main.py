f1bf198450a4d6724dbdfbe87dc7da61b6d73127
sofya <princessessonya@gmail.com>
2023-05-14 17:52:29 +0300

    Добавлено передвижение и прыжок

--- main.py
+++ main.py
@@ -25,6 +25,13 @@ walk_right = [
 player_anim_count = 0
 bg_x = 0
 
+player_speed = 5
+player_x = 150
+player_y = 330
+
+is_jump = False
+jump_count = 7
+
 bg_sound = pygame.mixer.Sound('sounds/bg.mp3')
 bg_sound.play()
 running = True
@@ -32,7 +39,34 @@ while running:
 
     screen.blit(bg, (bg_x, 0))
     screen.blit(bg, (bg_x + 511, 0))
-    screen.blit(walk_right[player_anim_count], (300, 330))
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        screen.blit(walk_left[player_anim_count], (player_x, player_y))
+    else:
+        screen.blit(walk_right[player_anim_count], (player_x, player_y))
+
+    if keys[pygame.K_LEFT] and player_x > 50:
+        player_x -= player_speed
+    elif keys[pygame.K_RIGHT] and player_x < 200:
+        player_x += player_speed
+
+
+    if not is_jump:
+        if keys[pygame.K_SPACE]:
+            is_jump =True
+    else:
+        if jump_count >= -7:
+           if jump_count >0:
+               player_y -= (jump_count ** 2) / 2
+           else:
+               player_y += (jump_count ** 2) / 2
+           jump_count -= 1
+        else:
+            is_jump = False
+            jump_count = 7
+
     if player_anim_count == 3:
         player_anim_count = 0
     else:
