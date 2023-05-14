ac354ab51bc092256fa86ac2a763080591a2f3d9
sofya <princessessonya@gmail.com>
2023-05-14 17:27:16 +0300

    Добавление анимации, звука, спрайтов

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
 import pygame
 
+clock = pygame.time.Clock()
 pygame.init()
-screen = pygame.display.set_mode((600, 300)) # flags=pygame.NOFRAME
+screen = pygame.display.set_mode((511, 513)) # flags=pygame.NOFRAME
 pygame.display.set_caption("Krutaya igra")
 icon = pygame.image.load('images/ikonka.png')
 pygame.display.set_icon(icon)
 
-square = pygame.Surface((50, 170))
-square.fill('Blue')
+bg = pygame.image.load('images/bg.png')
+walk_left = [
+    pygame.image.load('images/player_left/player_left1.png'),
+    pygame.image.load('images/player_left/player_left2.png'),
+    pygame.image.load('images/player_left/player_left3.png'),
+    pygame.image.load('images/player_left/player_left4.png'),
+]
 
-myfont = pygame.font.Font('fonts/Undertale-Battle-Font.ttf', 40)
-text_surface = myfont.render('Привет! Это игра.', True, 'White')
-player = pygame.image.load('images/ikonka.png')
+walk_right = [
+    pygame.image.load('images/player_right/player_right1.png'),
+    pygame.image.load('images/player_right/player_right2.png'),
+    pygame.image.load('images/player_right/player_right3.png'),
+    pygame.image.load('images/player_right/player_right4.png'),
+]
+
+player_anim_count = 0
+bg_x = 0
+
+bg_sound = pygame.mixer.Sound('sounds/bg.mp3')
+bg_sound.play()
 running = True
 while running:
 
-    pygame.draw.circle(screen, 'Red', (10, 7), 5)
-    screen.blit(square, (10, 0))
-    screen.blit(text_surface, (300, 100))
-    screen.blit(player, (20, 5))
+    screen.blit(bg, (bg_x, 0))
+    screen.blit(bg, (bg_x + 511, 0))
+    screen.blit(walk_right[player_anim_count], (300, 330))
+    if player_anim_count == 3:
+        player_anim_count = 0
+    else:
+        player_anim_count += 1
+
+    bg_x -= 2
+    if bg_x == -618:
+        bg_x = 0
     pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+
+
+
+    clock.tick(10)
